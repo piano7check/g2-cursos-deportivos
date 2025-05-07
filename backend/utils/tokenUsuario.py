df24b854e24e1a11d266f147d97199a834720f69
Alvaro Rivera <alvarorivera4513@gmail.com>
2025-05-07 13:10:16 -0400

    Generacion de token para la validacion de login

---
+++ backend/utils/tokenUsuario.py
@@ -0,0 +1,19 @@
+import jwt
+import datetime
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+def generarToken(usuario):
+    payload={
+        "id" : usuario['id'],
+        "name": usuario['name'],
+        "email": usuario['email'],
+        "rol": usuario["rol"],
+        "exp": datetime.datetime.utcnow()+ datetime.timedelta(hours=2)
+    }
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    return token
+    
