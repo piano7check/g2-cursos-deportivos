9ce26abf13737c3e71ae032a820f1b79d6d63631
Alvaro Rivera <alvarorivera4513@gmail.com>
2025-05-25 10:20:06 -0400

    Creacion del Esquema de Cursos

--- backend/schemas/cursos.py
+++ backend/schemas/cursos.py
@@ -1,5 +1,12 @@
-from cerberus import validator
+from cerberus import Validator
 
-validacionCursos = {
-    'nombre'
+esquemaCurso = {
+    'nombre' : {'type': 'string', 'maxlength': 100},
+    'descripcion': {'type': 'string'},
+    'cupos': {'type': 'integer'}
 }
+
+def validarCurso(data):
+    v = Validator(esquemaCurso)
+    esValido = v.validate(data)
+    return esValido, v.errors
