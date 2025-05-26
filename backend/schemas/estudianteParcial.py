c85c9edee83c084f5fd7d3449df78dbbf3c4c16a
Alvaro Rivera <alvarorivera4513@gmail.com>
2025-05-26 11:12:03 -0400

    feat: Nuevo esquema para validaciones parciales al editar usuario

---
+++ backend/schemas/estudianteParcial.py
@@ -0,0 +1,21 @@
+from cerberus import Validator
+
+esquemaUsuarioParcial = {
+    'name': {'type': 'string', 'minlength': 2, 'maxlength': 50},
+    'lastname': {'type': 'string', 'minlength': 2, 'maxlength': 50},
+    'birthdate': {
+        'type': 'string',
+        'regex': r'^\d{4}-\d{2}-\d{2}$'
+    },
+    'password': {
+        'type': 'string',
+        'minlength': 8,
+        'maxlength': 32,
+        'regex': r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[^\s]{8,32}$'
+    }
+}
+
+def validarUsuarioParcial(data):
+    v = Validator(esquemaUsuarioParcial, require_all=False, purge_unknown=True)
+    esValido = v.validate(data)
+    return esValido, v.errors, v.document 
