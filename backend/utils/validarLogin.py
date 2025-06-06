859085bc74dcd3bf3cf29f657591e84d23bc96db
Alvaro Rivera <alvarorivera4513@gmail.com>
2025-06-06 09:37:02 -0400

    test(backend): enviar name y lastname

--- backend/utils/validarLogin.py
+++ backend/utils/validarLogin.py
@@ -5,17 +5,17 @@ def validacionLogin(data):
         conexion = obtenerConexion()
         with conexion.cursor() as cursor:
             
-            consulta = "SELECT id, name, email, password, rol FROM users where email = %s"      
+            consulta = "SELECT id, name, lastname, email, password, rol FROM users where email = %s"      
             cursor.execute(consulta,(data['email'],))  
             usuario = cursor.fetchone()
             if 'email' not in data or 'password' not in data:
                 return {"error": "Faltan campos obligatorios"}
             if usuario:
-                id_usuario,user_name, user_email, password_user, rol = usuario
+                id_usuario,user_name, user_lastname ,user_email, password_user, rol = usuario
                 password_input = data['password'].encode('utf-8')
                 password_db = password_user.encode('utf-8')
                 if bcrypt.checkpw(password_input,password_db):
-                    return {"id":id_usuario, "name" : user_name, "email": user_email, "rol": rol}
+                    return {"id":id_usuario, "name" : user_name, "lastname": user_lastname,"email": user_email, "rol": rol}
                 else:
                     return {"error": "Contraseña incorrecta"}
             else:
