692890a7588099dce46928ec84ce537153d05c8f
AlvaroRiveraCarhuani <alvarorivera4513@gmail.com>
2025-06-20 14:38:55 -0400

    query para la creacion de la bd

---
+++ tests/schema.sql
@@ -0,0 +1,76 @@
+DROP TABLE IF EXISTS asistencias;
+DROP TABLE IF EXISTS validaciones_pago;
+DROP TABLE IF EXISTS reservas;
+DROP TABLE IF EXISTS horarios;
+DROP TABLE IF EXISTS cursos;
+DROP TABLE IF EXISTS categorias;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `birthdate` date NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` enum('estudiante','profesor','admin') NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `categorias` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `cursos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` text,
+  `cupos` int NOT NULL,
+  `profesor_id` int NOT NULL,
+  `categoria_id` int,
+  `coste` DECIMAL(10, 2) NOT NULL DEFAULT 0.00, 
+  PRIMARY KEY (`id`),
+  KEY `profesor_id` (`profesor_id`),
+  KEY `fk_categoria` (`categoria_id`), 
+  CONSTRAINT `cursos_ibfk_1` FOREIGN KEY (`profesor_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`) ON DELETE SET NULL 
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `horarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `curso_id` int NOT NULL,
+  `dia` enum('Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo') NOT NULL,
+  `hora_inicio` time NOT NULL,
+  `hora_fin` time NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `curso_id` (`curso_id`),
+  CONSTRAINT `horarios_ibfk_1` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `reservas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `estudiante_id` int NOT NULL,
+  `curso_id` int NOT NULL,
+  `fecha_reserva` datetime NOT NULL,
+  `estado` enum('pendiente','validado','expirado','cancelado') NOT NULL DEFAULT 'pendiente', 
+  `oculto_para_estudiante` BOOLEAN NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`id`),
+  KEY `estudiante_id` (`estudiante_id`),
+  KEY `curso_id` (`curso_id`),
+  CONSTRAINT `reservas_ibfk_1` FOREIGN KEY (`estudiante_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `reservas_ibfk_2` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `validaciones_pago` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `reserva_id` int NOT NULL,
+  `archivo_url` TEXT NOT NULL, 
+  `fecha_envio` datetime NOT NULL,
+  `estado` enum('pendiente','aprobado','rechazado') DEFAULT 'pendiente',
+  PRIMARY KEY (`id`),
+  KEY `reserva_id` (`reserva_id`),
+  CONSTRAINT `validaciones_pago_ibfk_1` FOREIGN KEY (`reserva_id`) REFERENCES `reservas` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
